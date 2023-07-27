f507ab7502e90c2fc023f5da7b2f38bb285fc0ef
viniciusfdo <viniciusfo12@gmail.com>
2023-07-26 22:35:05 -0300

    o

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,7 +1,8 @@
 public class Operadores {
 
     public static void main (String[] args){
-        
+        String nomeCompleto = "LINGUAGEM" + "JAVA";
+        System.out.println(nomeCompleto);
     }
     
 }
